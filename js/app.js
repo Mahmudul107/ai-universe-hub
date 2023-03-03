972736429afb50f67d818e5d1ca81ddbac2fb85f
Mahmudul107 <mahmudulislam378@gmail.com>
2023-03-03 10:37:24 +0600

    spinner or loader section added

--- js/app.js
+++ js/app.js
@@ -8,6 +8,10 @@ const loadFeatures = async() =>{
 
 const displayFeatures = features =>{
     const featuresContainer = document.getElementById('features-container');
+    // featuresContainer.textContent = '';
+
+    // slice feature items
+    features = features.slice(0,6);
     features.forEach(feature =>{
         const featureDiv = document.createElement('div');
         featureDiv.classList.add('col');
@@ -16,15 +20,34 @@ const displayFeatures = features =>{
             <img src="${feature.image}" class="card-img-top rounded-3" alt="...">
             <div class="card-body">
                 <h5 class="card-title">Features</h5>
-                <p class="card-text text-muted fw-light">${feature.features}</p>
+                <p class="features card-text text-muted fw-light">${feature.features}</p>
             </div>
-            <div class="card-footer">
-                <strong class="">${feature.name}</strong>
+            <div class="gap-5 d-flex justify-content-between align-items-center">
+                <div class="card-footer mt-2">
+                    <strong class="feature-name">${feature.name}</strong>
+                    <p class="fav-icon text-muted"> <i class="fa-solid fa-calendar-days"></i> ${feature.published_in}</p>
+                </div>
+                <div>
+                    <button class="arrow rounded-pill"><i class="fa-solid fa-arrow-right"></i></button>
+                </div>
             </div>
         </div>
         `;
         featuresContainer.appendChild(featureDiv);
     })
+    // Stop Spinner
+    toggleSpinner(false);
 }
 
+const toggleSpinner = isLoading =>{
+    const spinnerSection = document.getElementById('spinner');
+    if(isLoading){
+        spinnerSection.classList.remove('d-none');
+    }
+    else{
+        spinnerSection.classList.add('d-none');
+    }
+}
+
+toggleSpinner(true);
 loadFeatures();
