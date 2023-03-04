f54edfa280db7bddd638839f965387c6693f6823
Mahmudul107 <mahmudulislam378@gmail.com>
2023-03-04 11:45:26 +0600

    successfully add details left section

--- js/app.js
+++ js/app.js
@@ -1,23 +1,23 @@
 // Load data from API
-const loadFeatures = async() =>{
+const loadFeatures = async () => {
     const url = `https://openapi.programming-hero.com/api/ai/tools`
     const res = await fetch(url);
     const data = await res.json();
     displayFeatures(data.data.tools);
 }
 
-const displayFeatures = features =>{
+const displayFeatures = features => {
     const featuresContainer = document.getElementById('features-container');
     // featuresContainer.textContent = '';
 
     // slice feature with 6 items
     const seeMore = document.getElementById('see-more-section');
-    if(features.length > 6){
-        features = features.slice(0,6);
+    if (features.length > 6) {
+        features = features.slice(0, 6);
         seeMore.classList.remove('d-none');
     }
 
-    features.forEach(feature =>{
+    features.forEach(feature => {
         const featureDiv = document.createElement('div');
         featureDiv.classList.add('col');
         featureDiv.innerHTML = `
@@ -33,31 +33,31 @@ const displayFeatures = features =>{
                     <p class="fav-icon text-muted"> <i class="fa-solid fa-calendar-days"></i> ${feature.published_in}</p>
                 </div>
                 <div>
-                    <button onclick="showDetails('${feature.id}')" data-bs-toggle="modal" data-bs-target="#arrowDetailModal" class="arrow rounded-pill"><i class="fa-solid fa-arrow-right"></i></button>
+                    <button onclick="showDetails('${feature.id}')" data-bs-toggle="modal" data-bs-target="#featureDetailModal" class="arrow rounded-pill"><i class="fa-solid fa-arrow-right"></i></button>
                 </div>
             </div>
             </div>
             `;
-            featuresContainer.appendChild(featureDiv);
-        })
+        featuresContainer.appendChild(featureDiv);
+    })
     // Stop Spinner
     toggleSpinner(false);
 }
 
-const toggleSpinner = isLoading =>{
+const toggleSpinner = isLoading => {
     const spinnerSection = document.getElementById('spinner');
-    if(isLoading){
+    if (isLoading) {
         spinnerSection.classList.remove('d-none');
     }
-    else{
+    else {
         spinnerSection.classList.add('d-none');
     }
 }
 
- // Add click handler with button See More
-const restItem = document.getElementById('see-more-btn').addEventListener('click', function(){
+// Add click handler with button See More
+const restItem = document.getElementById('see-more-btn').addEventListener('click', function () {
     // loadFeatures();
- })
+})
 
 // Start spinner
 toggleSpinner(true);
@@ -65,45 +65,59 @@ toggleSpinner(true);
 loadFeatures();
 
 
-const showDetails = async id =>{
+const showDetails = async id => {
     const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
-    const res = await fetch (url);
+    const res = await fetch(url);
     const data = await res.json();
     displayDetails(data.data);
 }
 
-displayDetails = feature =>{
-    console.log(feature);  
-    const modalDescription = document.getElementById('arrowDetailModalLabel');
-    modalDescription.innerText = feature.description;
-    const modalContainer = document.getElementById('modal-container');
-    modalContainer.innerHTML = `
-        <div class="d-flex gap-5">
-            <p class="green">${feature.pricing[0].price}</p>
-            <p class="orange">${feature.pricing[1].price}</p>
-            <p class="red">${feature.pricing[2].price}</p>
-        </div>
-        <div class="container px-5 d-flex gap-5 justify-content-between">
-            <p>Features</p>
-            <p>Integration</p>
-        </div>
-        <div class="d-flex gap-5">
-            <div>
-                <ul>
-                    <li>${feature.features[1].feature_name}</li>
-                    <li>${feature.features[2].feature_name}</li>
-                    <li>${feature.features[3].feature_name}</li>
-                </ul>
+displayDetails = details => {
+    console.log(details);
+    const detailsContainer = document.getElementById('details-container');
+    detailsContainer.classList.add('col');
+    detailsContainer.innerHTML = `
+    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <div class="d-flex g-5">
+            <div class="details-section-container container ps-5">
+                <h1 class="details-description">${details.description}</h1>
+                <div class="d-flex gap-5">
+                    <p class="green">${details.pricing[0].price}</p>
+                    <p class="orange">${details.pricing[1].price}</p>
+                    <p class="red">${details.pricing[2].price}</p>
+                </div>
+                <div class="container px-5 d-flex gap-5 justify-content-between">
+                    <p>Features</p>
+                    <p>Integration</p>
+                </div>
+                <div class="d-flex gap-5">
+                    <div>
+                        <ul>
+                            <li>${details.features[1].feature_name}</li>
+                            <li>${details.features[2].feature_name}</li>
+                            <li>${details.features[3].feature_name}</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul>
+                            <li>${details.integrations[0]}</li>
+                            <li>${details.integrations[1]}</li>
+                            <li>${details.integrations[2]}</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-             <div>
-                <ul>
-                    <li>${feature.integrations[0]}</li>
-                    <li>${feature.integrations[1]}</li>
-                    <li>${feature.integrations[2]}</li>
-                </ul>
+            <div class="image-section ps-5 ">
+                <div>
+                    <img class="details-image" src="${details.image_link[0]}" alt="">
+                </div>
+                <div>
+                    
+                </div>
             </div>
         </div>
+        
     `
-}
 
+}
 // showDetails();
